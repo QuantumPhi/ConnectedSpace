adbb709f2c1d6d5a38f2836f98b065b2ac88c3c6
Tarik Onalan <phi.quantum@gmail.com>
2014-06-30 11:03:24 -0700

    More tweaks to laser scaling

--- core/src/space/entity/Laser.java
+++ core/src/space/entity/Laser.java
@@ -8,7 +8,7 @@ public class Laser extends Projectile {
     public Laser(double x, double y) {
         super(new Texture("laser.png"),x,y,7.5,5);
         sprite.setSize(sprite.getWidth()/3/1080f*Gdx.graphics.getWidth(),
-                sprite.getHeight()/1920f*Gdx.graphics.getHeight());
+                sprite.getHeight()*1.1f/1920f*Gdx.graphics.getHeight());
         sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
         sprite.setX((float)getScreenX() - sprite.getWidth() / 2);
     }
