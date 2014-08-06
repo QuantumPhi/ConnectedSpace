b0105d9e61784fdbf825499d0b48fff1e24787fd
Tarik Onalan <phi.quantum@gmail.com>
2014-08-06 23:31:19 +0300

    Scrapped old menu

---
+++ android/src/space/android/MainActivity.java
@@ -0,0 +1,15 @@
+package space.android;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
